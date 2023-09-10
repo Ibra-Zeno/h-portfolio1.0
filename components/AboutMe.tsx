87237a6b849cd3404751b9497601f7b6a65ad13d
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-09-10 22:17:47 +0100

    Image change

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -58,7 +58,7 @@ export default function About() {
           </div>
           <Image
             src={
-              "https://images.unsplash.com/photo-1694071132086-ba14a2071b60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+              "https://images.unsplash.com/photo-1693856758403-bba589db78dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
             }
             alt="Product screenshot"
             className="aspect-video w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
