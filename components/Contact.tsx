de4ea9b839063a07403524f922bc14ed7688f00c
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2024-05-31 21:57:26 +0100

    Removed extra code

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -63,18 +63,6 @@ const Contact: React.FC = () => {
               View CV
             </Button>
           </div>
-
-          {/* <div className="rounded-lg bg-blue-50/50 p-4 dark:bg-gray-800 md:p-6">
-            <div className="inline-block  rounded-lg bg-blue-100/80 p-2 text-blue-500 dark:bg-gray-700">
-              <Mail className="" />
-            </div>
-            <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">
-              Email
-            </h2>
-            <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">
-              <Link href="mailto:haranya.y@gmail.com">haranya.y@gmail.com</Link>
-            </p>
-          </div> */}
         </div>
       </div>
     </section>
@@ -82,56 +70,3 @@ const Contact: React.FC = () => {
 };
 
 export default Contact;
-
-/* import Link from "next/link";
-import { Building, Mail } from "lucide-react";
-import Image from "next/image";
-
-const Contact: React.FC = () => {
-  return (
-    <section className="">
-      <div className=" mx-auto flex h-fit flex-col py-20 md:grid md:h-full md:grid-cols-2">
-        <div
-          id="left-contact"
-          className="relative h-full w-full bg-gradient-to-tr from-lightBg to-transparent dark:from-darkBg  md:col-span-1"
-        >
-          <Image
-            src="/images/Hexagon.svg"
-            layout="fill"
-            alt="Descriptive Image Text"
-            className="absolute inset-0 -z-0 h-full object-cover opacity-30"
-          />
-
-          <div className=" relative z-10 flex h-full w-full  flex-col  justify-center bg-gradient-radial from-transparent via-lightBg to-lightBg px-8 text-center dark:via-darkBg dark:to-darkBg md:text-left ">
-            <div className="mx-auto my-auto lg:w-4/5 xl:w-3/5">
-              <h3 className="mb-4 text-xl font-semibold md:text-3xl lg:text-4xl">
-                Get in touch
-              </h3>
-              <p className="mb-6 text-sm sm:text-base md:mb-14 xl:text-lg">
-                I am currently looking for new opportunities, my inbox is always
-                open. Whether you have a question or just want to say hi,
-                I&apos;ll try my best to get back to you!
-              </p>
-              <div className="mb-6 flex flex-col gap-y-3 md:mb-0">
-                <div className="flex flex-row justify-center gap-x-3 text-sm font-medium md:justify-start md:text-base">
-                  <Building />
-                  <p>London, UK</p>
-                </div>
-                <div className="flex flex-row justify-center gap-x-3 text-sm font-medium md:justify-start md:text-base">
-                  <Mail />
-                  <Link href="mailto:haranya.y@gmail.com">
-                    haranya.y@gmail.com
-                  </Link>
-                </div>
-                <div></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default Contact;
- */
